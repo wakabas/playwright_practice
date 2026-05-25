458247f1dddcaaeb994425696a55769071326d93
wakabas <benyalol12@gmail.com>
2026-05-25 15:11:51 +0300

    refactor: упростил логику создания экземпляра класса

--- models/conf/config_reader.py
+++ models/conf/config_reader.py
@@ -6,12 +6,7 @@ class ConfigReader:
     _filename = None
 
     def __new__(cls, filename: str):
-        if cls._filename is None:
-            cls._instance = super().__new__(cls)
-            cls.read_config(filename)
-        if filename == cls._filename:
-            return cls._instance
-        else:
+        if cls._filename is None or cls._filename != filename:
             cls._instance = super().__new__(cls)
             cls.read_config(filename)
         return cls._instance
@@ -19,4 +14,5 @@ class ConfigReader:
     @classmethod
     def read_config(cls, filename: str) -> None:
         with open(filename, mode="r", encoding="utf-8") as f:
+            cls._filename = filename
             cls._instance.config = json.load(f)
