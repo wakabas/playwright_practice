ac73e259d174243ac9d9303e0374e78ea5b27326
wakabas <benyalol12@gmail.com>
2026-06-05 13:16:34 +0300

    refactor: поправил значение метода slider_values

--- tests/test_slider.py
+++ tests/test_slider.py
@@ -8,7 +8,7 @@ from pages.slider_page import SliderPage
 def test_slider(page: Page, base_url: str, endpoint):
     slider_page = SliderPage(page)
     slider_page.action.goto(f"{base_url}{endpoint[slider_page.__class__.__name__]}")
-    slider_value = choice(slider_page.get_slider_values)
+    slider_value = choice(slider_page.slider_values)
     num_of_press = int(slider_value * 2)
     slider_page.move_slider(num_of_press)
     actual_slider_value = slider_page.slider_value.get_inner_text()
